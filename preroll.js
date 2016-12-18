6e8edbb7770f030f245f90b1d43d52fb279c769b
Hayden Reid <haydenr@catalyst.net.nz>
2016-12-19 11:36:04 +1300

    Load poster image of main video initially.

--- preroll.js
+++ preroll.js
@@ -2,40 +2,46 @@
  * MTS prerolls.
  */
 videojs.plugin('mtsPreroll', function() {
-    var myPlayer = this,
-        prerollVideo,
-        mainVideo;
+  var myPlayer = this,
+    prerollVideo,
+    mainVideo;
 
-    function loadPreroll() {
-      prerollVideo = prerollVideo || player.options()['data-preroll-video-id'];
-      myPlayer.catalog.getVideo(prerollVideo, function (error, video) {
-        if (!error) {
-          myPlayer.catalog.load(video);
-        }
-      });
-    }
-
-    myPlayer.on("pause", function() {
-      myPlayer.bigPlayButton.show();
-    }); 
-
-    function playVideo() {
-      myPlayer.bigPlayButton.hide();
-      mainVideo = mainVideo || player.options()['data-main-video-id'];
-      myPlayer.catalog.getVideo(mainVideo, function (error, video) {
-        if (!error) {
-          myPlayer.catalog.load(video);
-          window.setTimeout(function() {
-            myPlayer.play();
-          }, 800);
-        }
-      });
-    }
-
-    myPlayer.one("ended", function () {
-        playVideo();
+  function loadPreroll() {
+    prerollVideo = prerollVideo || player.options()['data-preroll-video-id'];
+    myPlayer.catalog.getVideo(prerollVideo, function (error, video) {
+      if (!error) {
+        myPlayer.catalog.load(video);
+      }
     });
+    // Set the poster image of the main video.
+    mainVideo = mainVideo || player.options()['data-main-video-id'];
+    myPlayer.catalog.getVideo(mainVideo, function (error, video) {
+      if (!error) {
+        myPlayer.poster(video.poster);
+      }
+    });
+  }
 
-    loadPreroll();
+  myPlayer.on('pause', function() {
+    myPlayer.bigPlayButton.show();
+  });
+
+  function playVideo() {
+    myPlayer.bigPlayButton.hide();
+    mainVideo = mainVideo || player.options()['data-main-video-id'];
+    myPlayer.catalog.getVideo(mainVideo, function (error, video) {
+      if (!error) {
+        myPlayer.catalog.load(video);
+        window.setTimeout(function() {
+          myPlayer.play();
+        }, 800);
+      }
+    });
+  }
+
+  myPlayer.one('ended', function () {
+    playVideo();
+  });
+
+  loadPreroll();
 });
-
