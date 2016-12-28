6b686af5c6090717550808d87357c029a4d1df87
Hayden Reid <haydenr@catalyst.net.nz>
2016-12-29 12:57:24 +1300

    Custom end screens.

--- preroll.js
+++ preroll.js
@@ -3,19 +3,26 @@
  */
 videojs.plugin('mtsPreroll', function() {
   var myPlayer = this,
+    preroll = true,
     prerollVideo,
     mainVideo;
 
   function loadPreroll() {
     prerollVideo = prerollVideo || player.options()['data-preroll-video-id'];
+    if (prerollVideo == null) {
+      preroll = false;
+      return;
+    }
     mainVideo = mainVideo || player.options()['data-main-video-id'];
     myPlayer.catalog.getVideo(prerollVideo, function (error, video) {
       if (!error) {
+        console.log('Loading preroll video.');
         myPlayer.catalog.load(video);
         myPlayer.poster(null);
         // Set the poster image of the main video.
         myPlayer.catalog.getVideo(mainVideo, function (error, video) {
           if (!error) {
+            console.log('Loading original poster.');
             myPlayer.poster(video.poster);
           }
         });
@@ -40,8 +47,21 @@ videojs.plugin('mtsPreroll', function() {
     });
   }
 
-  myPlayer.one('ended', function () {
-    playVideo();
+  myPlayer.on('ended', function () {
+    if (preroll) {
+      preroll = false;
+      playVideo();
+    } else {
+      var language = myPlayer.language_,
+        message;
+      if (language === 'en') {
+        message = 'Thanks (EN).';
+      }
+      else {
+        message = 'Thanks (MI).';
+      }
+      jQuery('.html5-video-player').append('<div class="mts-endscreen vjs-modal-dialog" tabindex="-1" aria-describedby="video-player_endscreen" aria-hidden="false" aria-label="End screen" role="dialog"><div class="vjs-close-button vjs-control vjs-button" tabindex="0" role="button" aria-live="polite" aria-disabled="false" title="Close Modal Dialog" onclick="jQuery(\'.mts-endscreen\').hide();"><span class="vjs-control-text">Close Modal Dialog</span></div><p class="vjs-modal-dialog-description vjs-offscreen" id="video-player_endscreen">This is a modal window. This modal can be closed by activating the close button.</p><div class="vjs-modal-dialog-content" role="document"><p>' + message + '</p><p>Share: ' + window.location.href + '</p></div></div>');
+    }
   });
 
   loadPreroll();
