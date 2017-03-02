04e2e843558e0d97c3dfb7bbc245e1651156f44b
Hayden Reid <haydenr@catalyst.net.nz>
2017-03-02 17:17:59 +1300

    Update plugin for playlist play.

--- playlist-player.js
+++ playlist-player.js
@@ -7,10 +7,23 @@ videojs.plugin('mtsPlaylists', function() {
     return;
   }
 
-  var videos = JSON.parse(options['data-videos']);
+  var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+      var p=a[i].split('=', 2);
+      if (p.length == 1)
+        b[p[0]] = "";
+      else
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+  })(window.location.search.substr(1).split('&'));
 
-  var myPlayer = this,
-      i = 0;
+  var id = jQuery('ol[data-video-id="' + qs.id + '"]').attr('id');
+  var videos = JSON.parse(options['data-videos']);
+  var myPlayer = this;
 
   // Listen for the 'ended' event and play the next video.
   myPlayer.on('ended', function() {
@@ -34,6 +47,13 @@ videojs.plugin('mtsPlaylists', function() {
       if (!error) {
         // Change the name of the video being played.
         jQuery('h2#video-name').html(currentVideo.title);
+
+        // Change the share info.
+        jQuery('.mts-social-share-links li.mts-social-share-facebook a').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + currentVideo.url);
+        jQuery('.mts-social-share-links li.mts-social-share-twitter a').attr('href', 'https://twitter.com/intent/tweet?original_referer=' + currentVideo.url + '&tw_p=tweetbutton&via=maoritv&text=' + currentVideo.title + '&url=' + currentVideo.url);
+        jQuery('.mts-social-share-links li.mts-social-share-email a').attr('href', 'mailto:?to=&subject=' + currentVideo.title + '&body=' + currentVideo.url);
+        jQuery('.mts-social-share-links li.mts-social-share-copy-link input.copy-link-processed').val(currentVideo.url);
+
         myPlayer.catalog.load(video);
         window.setTimeout(function() {
           myPlayer.play();
@@ -51,5 +71,5 @@ videojs.plugin('mtsPlaylists', function() {
   });
 
   // Play the first video to start with.
-  playVideo();
+  playVideo(id);
 });
