58f97ec0d830f369eff97860a534719bab6cd59e
Hayden <klidifia@hotmail.com>
2018-05-14 09:50:54 +1200

    Match with local
    Match with local

--- custom.js
+++ custom.js
@@ -1,36 +1,54 @@
 /**
- * MTS Custom plugin for preroll videos.
+ * MTS Custom plugin for playing videos in the Tamariki section.
+ * custom.js must be loaded first.
  */
 
-// Cross-compatibility for Video.js 5 and 6.
-var registerPlugin = videojs.registerPlugin || videojs.plugin;
-
-registerPlugin('mtsCustom', function() {
+registerPlugin('mtsTamariki', function() {
   var player = this;
   var options = player.options_;
-  // Exit if we are playing a playlist, or a Tamariki list.
-  if ((options.hasOwnProperty('data-videos') || options.hasOwnProperty('data-tamariki-videos'))) {
+  // Exit if we are not in the Tamariki section.
+  if (!options.hasOwnProperty('data-tamariki-videos')){
     return;
   }
 
-  var myPlayer = this,
-    preroll = true,
-    prerollVideo,
-    mainVideo,
-    dynamicDelivery;
+  var videos = JSON.parse(options['data-tamariki-videos']);
+  var myPlayer = this;
+  var videoIndex;
 
-  function loadPreroll() {
-    prerollVideo = prerollVideo || options['data-preroll-video-id'];
-    if (prerollVideo == null) {
-      preroll = false;
-      return;
+  // Listen for the 'ended' event and play the next video.
+  myPlayer.on('ended', function() {
+    videoIndex = jQuery('#videoindex').val();
+
+    var keysArray = Object.keys(videos);
+    var index = keysArray.indexOf(String(videoIndex));
+    index++;
+    var objectKey = keysArray[index];
+
+    // If we are at the end then show the custom endscreen.
+    if (objectKey === undefined) {
+      jQuery('#' + myPlayer.id_).append('<div class="mts-endscreen vjs-social-overlay vjs-modal-dialog" tabindex="-1" aria-describedby="video-player_endscreen" aria-hidden="false" aria-label="End screen" role="dialog"><button class="vjs-close-button vjs-control vjs-button" tabindex="0" role="button" aria-live="polite" aria-disabled="false" title="Close Modal Dialog" onclick="jQuery(\'#' + myPlayer.id_ + ' .mts-endscreen\').hide();"><span aria-hidden="true" class="vjs-icon-placeholder"></span><span class="vjs-control-text">Close Modal Dialog</span></button><p class="vjs-modal-dialog-description vjs-offscreen" id="video-player_endscreen">This is a modal window. This modal can be closed by activating the close button.</p><div class="vjs-modal-dialog-content" role="document"><img class="endscreen-header" src="/sites/all/themes/mts/images/endscreen-header-thanks.png" /><p class="endscreen-text"><a href="/tamariki">Back to Tamariki</a></p><img class="endscreen-footer" src="/sites/all/themes/mts/images/endscreen-footer-logo.jpg" /></div></div>');
     }
-    mainVideo = mainVideo || options['data-main-video-id'];
-    myPlayer.catalog.getVideo(prerollVideo, function (error, video) {
+    else {
+      // Record the video being played so we can continue with the next one.
+      jQuery('#videoindex').val(objectKey);
+
+      // Play the next video.
+      playVideo(objectKey);
+    }
+  });
+
+  function playVideo (videoIndex) {
+    // Hide the big play button since we are auto-playing.
+    myPlayer.bigPlayButton.hide();
+
+    // Determine what the next video ID is.
+    var currentVideo = videos[videoIndex];
+
+    // Load and play the next video.
+    myPlayer.catalog.getVideo(currentVideo.video_id, function (error, video) {
       if (!error) {
         // Add/remove video overlay depending if ingested via dynamic delivery.
-        dynamicDelivery = dynamicDelivery || options['data-dynamic-delivery'];
-        if (dynamicDelivery === '1') {
+        if (currentVideo.dynamic_delivery) {
           jQuery('.vjs-overlay-top-right').show();
         }
         else {
@@ -38,51 +56,36 @@ registerPlugin('mtsCustom', function() {
         }
 
         myPlayer.catalog.load(video);
-        myPlayer.poster(null);
-        // Set the poster image of the main video.
-        myPlayer.catalog.getVideo(mainVideo, function (error, video) {
-          if (!error) {
-            myPlayer.poster(video.poster);
-          }
-        });
-      }
-    });
-  }
-
-  myPlayer.on('pause', function() {
-    myPlayer.bigPlayButton.show();
-  });
-
-  function playVideo() {
-    myPlayer.bigPlayButton.hide();
-    mainVideo = mainVideo || options['data-main-video-id'];
-    myPlayer.catalog.getVideo(mainVideo, function (error, video) {
-      if (!error) {
-        myPlayer.catalog.load(video);
-        window.setTimeout(function() {
+        window.setTimeout(function () {
           myPlayer.play();
         }, 800);
       }
     });
   }
 
-  myPlayer.on('ended', function () {
-    if (preroll) {
-      preroll = false;
-      playVideo();
+  // When a video is clicked on, enter fullscreen and play the video.
+  jQuery( document ).on( "click", ".tamariki-video", function() {
+    jQuery('.mts-endscreen').remove();
+    jQuery('.video-js').show();
+    myPlayer.requestFullscreen();
+    videoIndex = jQuery(this).attr('id');
+
+    // If the same video was exited and entered again - resume the playback.
+    if (videoIndex === jQuery('#videoindex').val()) {
+      myPlayer.play();
     }
     else {
-      if (jQuery('#' + myPlayer.id_ + ' .mts-endscreen').length) {
-        return;
-      }
-      // WR#282109: Bail out if player has .no-endscreen class.
-      if (jQuery('#' + myPlayer.id_).hasClass('.no-endscreen')) {
-        return;
-      }
-      var share = 'Direct link<br /><input class="endscreen-direct-link" tabindex="7" readonly="true" value="' + window.location.href + '" type="text">';
-      jQuery('#' + myPlayer.id_).append('<div class="mts-endscreen vjs-social-overlay vjs-modal-dialog" tabindex="-1" aria-describedby="video-player_endscreen" aria-hidden="false" aria-label="End screen" role="dialog"><button class="vjs-close-button vjs-control vjs-button" tabindex="0" role="button" aria-live="polite" aria-disabled="false" title="Close Modal Dialog" onclick="jQuery(\'#' + myPlayer.id_ + ' .mts-endscreen\').hide();"><span aria-hidden="true" class="vjs-icon-placeholder"></span><span class="vjs-control-text">Close Modal Dialog</span></button><p class="vjs-modal-dialog-description vjs-offscreen" id="video-player_endscreen">This is a modal window. This modal can be closed by activating the close button.</p><div class="vjs-modal-dialog-content" role="document"><img class="endscreen-header" src="/sites/all/themes/mts/images/endscreen-header-thanks.png" /><div class="vjs-social-share-links"><a href="https://www.facebook.com/sharer/sharer.php?u=' + window.location.href + '" class="vjs-social-share-link vjs-icon-facebook" aria-role="link" aria-label="Share on Facebook" tabindex="1" title="Facebook" target="_blank"><span class="vjs-control-text">Facebook</span></a><a href="https://plus.google.com/share?url=' + window.location.href + '" class="vjs-social-share-link vjs-icon-gplus" aria-role="link" aria-label="Share on Google Plus" tabindex="2" title="Google+" target="_blank"><span class="vjs-control-text">Google+</span></a><a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=' + window.location.href + '&amp;summary=&amp;source=Classic" class="vjs-social-share-link vjs-icon-linkedin" aria-role="link" aria-label="Share on LinkedIn" tabindex="3" title="LinkedIn" target="_blank"><span class="vjs-control-text">LinkedIn</span></a><a href="https://pinterest.com/pin/create/button/?url=' + window.location.href + '&amp;is_video=true" class="vjs-social-share-link vjs-icon-pinterest" aria-role="link" aria-label="Share on Pinterest" tabindex="4" title="Pinterest" target="_blank"><span class="vjs-control-text">Pinterest</span></a><a href="http://www.tumblr.com/share?v=3&amp;u=' + window.location.href + '" class="vjs-social-share-link vjs-icon-tumblr" aria-role="link" aria-label="Share on Tumblr" tabindex="5" title="Tumblr" target="_blank"><span class="vjs-control-text">Tumblr</span></a><a href="https://twitter.com/intent/tweet?original_referer=' + window.location.href + '&amp;tw_p=tweetbutton&amp;url=' + window.location.href + '" class="vjs-social-share-link vjs-icon-twitter" aria-role="link" aria-label="Share on Twitter" tabindex="6" title="Twitter" target="_blank"><span class="vjs-control-text">Twitter</span></a></div><p class="endscreen-text">' + share + '</p><img class="endscreen-footer" src="/sites/all/themes/mts/images/endscreen-footer-logo.jpg" /></div></div>');
+      playVideo(videoIndex);
+      // Record the video being played so we can continue with the next one.
+      jQuery('#videoindex').val(videoIndex);
     }
   });
 
-  loadPreroll();
+  // When fullscreen is exited, pause and hide the video player.
+  myPlayer.on('fullscreenchange', function() {
+    if (!(myPlayer.isFullscreen())) {
+      myPlayer.pause();
+      jQuery('.video-js').hide();
+    }
+  });
 });
