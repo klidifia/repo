7ad44288f0b2d71705700955f3aa439f440a29df
Hayden <klidifia@hotmail.com>
2018-05-14 09:52:13 +1200

    Update
    update

--- tamariki.js
+++ tamariki.js
@@ -1,9 +1,11 @@
 /**
  * MTS Custom plugin for playing videos in the Tamariki section.
+ * custom.js must be loaded first.
  */
-videojs.plugin('mtsTamariki', function() {
+
+registerPlugin('mtsTamariki', function() {
   var player = this;
-  var options = player.options();
+  var options = player.options_;
   // Exit if we are not in the Tamariki section.
   if (!options.hasOwnProperty('data-tamariki-videos')){
     return;
@@ -24,7 +26,7 @@ videojs.plugin('mtsTamariki', function() {
 
     // If we are at the end then show the custom endscreen.
     if (objectKey === undefined) {
-      jQuery('#' + myPlayer.id_).append('<div class="mts-endscreen vjs-social-overlay vjs-modal-dialog" tabindex="-1" aria-describedby="video-player_endscreen" aria-hidden="false" aria-label="End screen" role="dialog"><div class="vjs-close-button vjs-control vjs-button" tabindex="0" role="button" aria-live="polite" aria-disabled="false" title="Close Modal Dialog" onclick="jQuery(\'#' + myPlayer.id_ + ' .mts-endscreen\').hide();"><span class="vjs-control-text">Close Modal Dialog</span></div><p class="vjs-modal-dialog-description vjs-offscreen" id="video-player_endscreen">This is a modal window. This modal can be closed by activating the close button.</p><div class="vjs-modal-dialog-content" role="document"><img class="endscreen-header" src="/sites/all/themes/mts/images/endscreen-header-thanks.png" /><p class="endscreen-text"><a href="/tamariki">Back to Tamariki</a></p><img class="endscreen-footer" src="/sites/all/themes/mts/images/endscreen-footer-logo.jpg" /></div></div>');
+      jQuery('#' + myPlayer.id_).append('<div class="mts-endscreen vjs-social-overlay vjs-modal-dialog" tabindex="-1" aria-describedby="video-player_endscreen" aria-hidden="false" aria-label="End screen" role="dialog"><button class="vjs-close-button vjs-control vjs-button" tabindex="0" role="button" aria-live="polite" aria-disabled="false" title="Close Modal Dialog" onclick="jQuery(\'#' + myPlayer.id_ + ' .mts-endscreen\').hide();"><span aria-hidden="true" class="vjs-icon-placeholder"></span><span class="vjs-control-text">Close Modal Dialog</span></button><p class="vjs-modal-dialog-description vjs-offscreen" id="video-player_endscreen">This is a modal window. This modal can be closed by activating the close button.</p><div class="vjs-modal-dialog-content" role="document"><img class="endscreen-header" src="/sites/all/themes/mts/images/endscreen-header-thanks.png" /><p class="endscreen-text"><a href="/tamariki">Back to Tamariki</a></p><img class="endscreen-footer" src="/sites/all/themes/mts/images/endscreen-footer-logo.jpg" /></div></div>');
     }
     else {
       // Record the video being played so we can continue with the next one.
@@ -45,6 +47,14 @@ videojs.plugin('mtsTamariki', function() {
     // Load and play the next video.
     myPlayer.catalog.getVideo(currentVideo.video_id, function (error, video) {
       if (!error) {
+        // Add/remove video overlay depending if ingested via dynamic delivery.
+        if (currentVideo.dynamic_delivery) {
+          jQuery('.vjs-overlay-top-right').show();
+        }
+        else {
+          jQuery('.vjs-overlay-top-right').hide();
+        }
+
         myPlayer.catalog.load(video);
         window.setTimeout(function () {
           myPlayer.play();
@@ -54,7 +64,7 @@ videojs.plugin('mtsTamariki', function() {
   }
 
   // When a video is clicked on, enter fullscreen and play the video.
-  jQuery('.tamariki-video').live('click', function () {
+  jQuery( document ).on( "click", ".tamariki-video", function() {
     jQuery('.mts-endscreen').remove();
     jQuery('.video-js').show();
     myPlayer.requestFullscreen();
